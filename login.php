c1a64ed7488734a9791d2d41da3f168d310ee4e5
vidan kukic <kukicvidan@gmail.com>
2024-04-22 16:51:43 +0200

    Added Login and Register pages and css for them
    Connected pages with #a and added styles for two new pages (Login and Register), also some small changes to index.php with some styling

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - codingnepalweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Login </title> 
+    <link rel="stylesheet" href="register.css">
+   </head>
+<body>
+  <div class="wrapper">
+    <h2>Login</h2>
+    <form action="#">
+     
+      <div class="input-box">
+        <input type="text" placeholder="Enter your email" required>
+      </div>
+      
+      <div class="input-box">
+        <input type="password" placeholder="Enter password" required>
+      </div>
+     
+      <div class="input-box button">
+        <input type="Submit" value="Login">
+      </div>
+      <div class="text">
+        <h3>Dont have account? <a href="register.php">register now</a></h3>
+      </div>
+      <div class="text">
+        <h3><a href="index.php">Back to homepage</a></h3>
+      </div>
+    </form>
+  </div>
+</body>
+</html>
